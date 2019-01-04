fb8d6dd5253b1fd6de58c2ba4bebdb978d936996
LucNieto <noodle3030@hotmail.com>
2019-01-03 23:57:22 -0600

    reset game button added

--- app/src/main/java/com/io/luc/helloworld/MainActivity.java
+++ app/src/main/java/com/io/luc/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package com.io.luc.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.support.v7.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -15,50 +17,97 @@ public class MainActivity extends AppCompatActivity {
     enum Player { ONE, TWO, NO }
     Player currentPlayer = Player.ONE;
     Player[] playerChoice = new Player[9];
+    private boolean gameOver = false;
+
+    private Button btnReset;
+
+    private GridLayout gridLayout;
 
     int[][] winningLines = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // for (int i=0; i< playerChoice.length; i++) {
-           // playerChoice[i] = Player.NO;
-        //}
-        playerChoice[0] = Player.NO;
-        playerChoice[1] = Player.NO;
-        playerChoice[2] = Player.NO;
-        playerChoice[3] = Player.NO;
-        playerChoice[4] = Player.NO;
-        playerChoice[5] = Player.NO;
-        playerChoice[6] = Player.NO;
-        playerChoice[7] = Player.NO;
-        playerChoice[8] = Player.NO;
+        PlayerChoices();
+        btnReset = findViewById(R.id.btnReset);
+        gridLayout = findViewById(R.id.gridLayout);
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                resetTheGame();
+            }
+        });
+
     }
 
 
     public  void  imageViewIsTapped(View tile){
         ImageView tappedTile = (ImageView) tile;
-        tappedTile.setTranslationX(-2000);
-        if(currentPlayer == Player.ONE){
-            tappedTile.setImageResource(R.drawable.lion);
-            currentPlayer = Player.TWO;
-        }else {
-            tappedTile.setImageResource(R.drawable.tiger);
-            currentPlayer = Player.ONE;
-        }
 
-        tappedTile.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+        int tiTag = Integer.parseInt(tappedTile.getTag().toString() );
+        if (playerChoice[tiTag] == Player.NO && gameOver == false) {
+            playerChoice[tiTag] = currentPlayer;
+            tappedTile.setTranslationX(-2000);
+            if (currentPlayer == Player.ONE) {
+                tappedTile.setImageResource(R.drawable.lion);
+                currentPlayer = Player.TWO;
+            } else {
+                tappedTile.setImageResource(R.drawable.tiger);
+                currentPlayer = Player.ONE;
+            }
 
-        for (int[] winnerLine : winningLines) {
-           // if(playerChoice[winnerLine[0]] != Player.NO){
-                if (    playerChoice[winnerLine[0]] == playerChoice[winnerLine[1]]
+            tappedTile.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+
+            for (int[] winnerLine : winningLines) {
+                 if(playerChoice[winnerLine[0]] != Player.NO){
+                if (playerChoice[winnerLine[0]] == playerChoice[winnerLine[1]]
                         && playerChoice[winnerLine[1]] == playerChoice[winnerLine[2]]
-                        && playerChoice[winnerLine[0]] != Player.NO) {
-                    Toast.makeText(this, "Winner Winner \nchicken dinner", Toast.LENGTH_SHORT).show();
-                }
-            //}
+                        /*&& playerChoice[winnerLine[0]] != Player.NO*/) {
+                    btnReset.setVisibility(View.VISIBLE);
+                    gameOver = true;
+                    String winnerOfGame = "";
 
+                    if (currentPlayer == Player.ONE) {
+
+                        winnerOfGame = "Player Two ";
+
+                    } else if (currentPlayer == Player.TWO) {
+
+                        winnerOfGame = "Player One ";
+
+
+                    }
+                    Toast.makeText(this, winnerOfGame + "is the Winner",
+                            Toast.LENGTH_LONG).show();
+                }
+                }
+
+            }
         }
 
+    }
+    private void resetTheGame() {
+
+        for (int index = 0; index < gridLayout.getChildCount(); index++) {
+
+            ImageView imageView = (ImageView) gridLayout.getChildAt(index);
+            imageView.setImageDrawable(null);
+            imageView.setAlpha(0.2f);
+        }
+
+        currentPlayer = Player.ONE;
+        PlayerChoices();
+
+
+        gameOver = false;
+
+    }
+
+    private void PlayerChoices(){
+        for (int i=0; i< playerChoice.length; i++) {
+            playerChoice[i] = Player.NO;
+        }
     }
 }
