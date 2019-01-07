94b2d478c4da348498f204af906bea8a14e61608
LucNieto <noodle3030@hotmail.com>
2019-01-07 16:46:38 -0600

    setting up the video player

--- app/src/main/java/com/io/luc/helloworld/MainActivity.java
+++ app/src/main/java/com/io/luc/helloworld/MainActivity.java
@@ -1,18 +1,35 @@
 package com.io.luc.helloworld;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 /**
  * @author Lucio Nieto Bautista
  * CEO at Nieto Industries
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private VideoView myVideoView;
+    private Button btnPlayVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myVideoView = findViewById(R.id.myVideoView);
+        btnPlayVideo = findViewById(R.id.btnPlayVideo);
+        btnPlayVideo.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+      //  Uri videoUri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.L);
     }
 }
