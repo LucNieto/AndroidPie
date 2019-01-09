838a146038631aae0a88f5cf1715119bcdfda85d
LucNieto <noodle3030@hotmail.com>
2019-01-08 22:26:48 -0600

    Audio and video player

--- app/src/main/java/com/io/luc/helloworld/MainActivity.java
+++ app/src/main/java/com/io/luc/helloworld/MainActivity.java
@@ -1,10 +1,12 @@
 package com.io.luc.helloworld;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 /**
@@ -16,6 +18,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private VideoView myVideoView;
     private Button btnPlayVideo;
+    private Button btnPlayMusic;
+    private Button btnPauseMusic;
+    private MediaPlayer mediaPlayer;
+
+    private MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +32,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         myVideoView = findViewById(R.id.myVideoView);
         btnPlayVideo = findViewById(R.id.btnPlayVideo);
         btnPlayVideo.setOnClickListener(MainActivity.this);
+        mediaController = new MediaController(MainActivity.this);
+        btnPlayMusic.setOnClickListener(MainActivity.this);
+        btnPauseMusic.setOnClickListener(MainActivity.this);
+        mediaPlayer = MediaPlayer.create(this, R.raw.the_grid);
     }
 
     @Override
     public void onClick(View v) {
+       // text animation
+       //TextView txt = findViewById(R.id.testTxt);
+       // txt.animate().scaleX(10);
 
-      //  Uri videoUri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.L);
+        switch (v.getId()){
+            case R.id.btnPlayVideo:
+                Uri videoUri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.tron);
+                myVideoView.setVideoURI(videoUri);
+                myVideoView.setMediaController(mediaController);
+                myVideoView.start();
+            break;
+            case R.id.btnPlayMusic:
+                mediaPlayer.start();
+            break;
+            case R.id.btnPauseMusic:
+                mediaPlayer.pause();
+                break;
+
+        }
+
+
     }
 }
